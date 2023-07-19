e96f962f79fa3f93b22e05c97f640b3807df8e33
Artyomk1 <artyom.k1@tumo.org>
2023-07-19 17:54:56 +0400

    New Photos

--- nkarner.js
+++ nkarner.js
@@ -1,15 +1,16 @@
 const Prezident = [
     {
        "Prezident": "./photo/Armenia.jpg",
-       "name": "Վահագն Խաչատուրյան",
+       "name": "Վահագն Խաչատուրյան",},
+    {
        "Prezident": "./photo/Afrika.jpg",
-       "name": "Cyril Ramaphosa",
+       "name": "Cyril Ramaphosa",},
+    {
        "Prezident": "./photo/Argentina.jpg",
-       "name": "Ալբերտո Ֆեռնանդես",
+       "name": "Ալբերտո Ֆեռնանդես",},
+    {
        "Prezident": "./photo/Avstralia.jpg",
-       "name": "Վահագն Խաչատուրյան",
-       
-       
-    }
+       "name": "Վահագն Խաչատուրյան",  
+    },
 
 ]
